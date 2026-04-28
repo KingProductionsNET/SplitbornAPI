bf9d5e06a2932b5a6bc5f59c69bc0aa14835a8be
BuildTools <unconfigured@null.spigotmc.org>
2026-04-29 00:21:17 +0200

    Initial commit

--- src/main/java/net/kingproductions/splitbornAPI/Profile.java
+++ src/main/java/net/kingproductions/splitbornAPI/Profile.java
@@ -1,6 +1,5 @@
 package net.kingproductions.splitbornAPI;
 
-<<<<<<< HEAD
 import java.util.UUID;
 
 public interface Profile {
@@ -8,12 +7,5 @@ public interface Profile {
     UUID getUUID();
 
     int getXP();
-=======
-public interface Profile {
-
-    int getXP();
-
->>>>>>> bc1f947b0e9f2959fd92fa13f7addf5bfa16eda5
     void setXP(int xp);
-
 }
