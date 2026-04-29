2b70aefe6c601c6a8a10f296debc0faf8ec74adb
BuildTools <unconfigured@null.spigotmc.org>
2026-04-30 00:00:16 +0200

    Initial commit

--- src/main/java/net/kingproductions/splitbornAPI/ProfileContainer/Profile.java
+++ src/main/java/net/kingproductions/splitbornAPI/ProfileContainer/Profile.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 public interface Profile {
 
     UUID getUUID();
-
     int getXP();
     void setXP(int xp);
+
+    boolean getBoolean(String Path);
+    boolean getString(String Path);
+    boolean getInteger(String Path);
 }
