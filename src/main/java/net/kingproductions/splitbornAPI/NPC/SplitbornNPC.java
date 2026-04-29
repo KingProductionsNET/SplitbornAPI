95deeece0bef89117cc599cc7eabd27eea43ced7
BuildTools <unconfigured@null.spigotmc.org>
2026-04-29 17:23:17 +0200

    Initial commit

--- src/main/java/net/kingproductions/splitbornAPI/NPC/SplitbornNPC.java
+++ src/main/java/net/kingproductions/splitbornAPI/NPC/SplitbornNPC.java
@@ -1,13 +1,10 @@
 package net.kingproductions.splitbornAPI.NPC;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-
-import java.util.UUID;
 
 public class SplitbornNPC {
 
-    private final NPC_ID id;
+    private final NPC_ID ID;
 
     private EntityType type = null;
 
@@ -15,7 +12,7 @@ public class SplitbornNPC {
     private String Signature = "lCurX5AoIFE+Lkn5s8/2Sx7DriYVPRkNh0my8TJvPbtozrbAhXGUsBfvVJnn4mjhErBGzf0OzLjPBYUXUeyHSqflRNjMsskcvJU1Kk6aeJilA6DpM8Ep19t6jKE5hu7kWAb8+q0sSc+4gS5BZaHAIXuEZPVImxbj5PdkjiZp2CKvCDyMCHl9lm0JNZwnL0lPPxb9WT+v9fhgWwzZlVgfevJCvssjs/tye9gi6tG0WtmK6P4PVJLJrj3m277abjRiIgXaMmytqpwhn24QiBHUj3MOCZH95GuAQaeknnz34ow9vgsbiTCr8ClUHw1E4fND42xGnuGOthAZmY/IycJr4bGEc3Idbm9xAA7Mq4SFCXi9/EAAu9Dj++WJ2FNCQZ9MZ6x3nJ/ybVL+6A7hsQ2/ew4qZi3aTb2fiDCgokyXtZhXX8CBqWy8l8gBJmwvTPLgvPloJMZ3F9CHu8h0qlorA0r9TphYunP5FX5KtYKfzCEr367l/XZMU8PE1xkY5iF2xFJB3wVFz7WvnnyyHJvLdH2I+EI3kMM+utf2qYgkhKphTtO87vIFzn6U6JhTDh/eZ93wdCzNoqbmz5WSyaR+VARFyEs7OdwNZaR3MpN46Ymx5u48fJZCmhzH5ouvL+wLjuI0j9ZZqkg1eC+2bhPPeqvwkMyxt6y9hwRcktiZ6fE=";
 
     public SplitbornNPC(NPC_ID npcId) {
-        this.id = npcId;
+        this.ID = npcId;
     }
     public SplitbornNPC setSkin(String value, String signature){
         Value = value;
@@ -32,5 +29,8 @@ public class SplitbornNPC {
     }
     public String getSkin_Signature(){
         return Signature;
+    }
+    public NPC_ID getID(){
+        return ID;
     }
 }
