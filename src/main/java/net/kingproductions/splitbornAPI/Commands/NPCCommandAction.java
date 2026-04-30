c0702e5cc3892339cd4008018e565c835931a94e
BuildTools <unconfigured@null.spigotmc.org>
2026-04-30 14:24:08 +0200

    Initial commit

--- src/main/java/net/kingproductions/splitbornAPI/Commands/NPCCommandAction.java
+++ src/main/java/net/kingproductions/splitbornAPI/Commands/NPCCommandAction.java
@@ -26,39 +26,39 @@ public class NPCCommandAction implements CommandExecutor {
             if (IS_CURRENTLY_IN_DIALOGUE.contains(player.getUniqueId())) return true;
 
             if (getPlayersDedicatedNCA.getOrDefault(player.getUniqueId(), "null").equalsIgnoreCase(SubCommand)){
-                if (SubCommand.equalsIgnoreCase(AREO_NCA.NEW_PART_2)){
-                    AREO_DIALOGUES.NEW_TALK_PART_2(player);
-                }
-                if (SubCommand.equalsIgnoreCase(AREO_NCA.NEW_PART_3)){
-                    AREO_DIALOGUES.NEW_TALK_PART_3(player);
-                }
-
-                if (SubCommand.equalsIgnoreCase(ANDREW_NCA.PART_2)){
-                    ANDREW_DIALOGUES.NEW_PART_2(player);
-                }
-
-                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_2)){
-                    STEPHEN_DIALOGUES.NEW_PART_2(player);
-                }
-                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_3)){
-                    STEPHEN_DIALOGUES.NEW_PART_3(player);
-                }
-                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_4)){
-                    STEPHEN_DIALOGUES.NEW_PART_4(player);
-                }
-                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_5)){
-                    STEPHEN_DIALOGUES.NEW_PART_5(player);
-                }
-                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_6)){
-                    STEPHEN_DIALOGUES.NEW_PART_6(player);
-                }
-
-                if (SubCommand.equalsIgnoreCase(BONSAI_NCA.NCA_COMMAND_BONSAI_DIALOGUE_PART_2)) {
-                    BONSAI_DIALOGUES.Part_2(player);
-                }
-                if (SubCommand.equalsIgnoreCase(GRUG_NCA.NCA_COMMAND_GRUG_BONSAI_QUEST_DIALOGUE_PART_2)){
-                    GRUG_DIALOGUES.Bonsai_Quest_Part_2(player);
-                }
+//                if (SubCommand.equalsIgnoreCase(AREO_NCA.NEW_PART_2)){
+//                    AREO_DIALOGUES.NEW_TALK_PART_2(player);
+//                }
+//                if (SubCommand.equalsIgnoreCase(AREO_NCA.NEW_PART_3)){
+//                    AREO_DIALOGUES.NEW_TALK_PART_3(player);
+//                }
+//
+//                if (SubCommand.equalsIgnoreCase(ANDREW_NCA.PART_2)){
+//                    ANDREW_DIALOGUES.NEW_PART_2(player);
+//                }
+//
+//                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_2)){
+//                    STEPHEN_DIALOGUES.NEW_PART_2(player);
+//                }
+//                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_3)){
+//                    STEPHEN_DIALOGUES.NEW_PART_3(player);
+//                }
+//                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_4)){
+//                    STEPHEN_DIALOGUES.NEW_PART_4(player);
+//                }
+//                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_5)){
+//                    STEPHEN_DIALOGUES.NEW_PART_5(player);
+//                }
+//                if (SubCommand.equalsIgnoreCase(STEPHEN_NCA.NEW_PART_6)){
+//                    STEPHEN_DIALOGUES.NEW_PART_6(player);
+//                }
+//
+//                if (SubCommand.equalsIgnoreCase(BONSAI_NCA.NCA_COMMAND_BONSAI_DIALOGUE_PART_2)) {
+//                    BONSAI_DIALOGUES.Part_2(player);
+//                }
+//                if (SubCommand.equalsIgnoreCase(GRUG_NCA.NCA_COMMAND_GRUG_BONSAI_QUEST_DIALOGUE_PART_2)){
+//                    GRUG_DIALOGUES.Bonsai_Quest_Part_2(player);
+//                }
 
                 getPlayersDedicatedNCA.remove(player.getUniqueId());
             } else {
