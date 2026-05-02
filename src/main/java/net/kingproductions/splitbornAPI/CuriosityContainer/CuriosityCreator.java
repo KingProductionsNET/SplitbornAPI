289b18f34c512013a4eda2ea023f802fb34a22b8
BuildTools <unconfigured@null.spigotmc.org>
2026-05-02 18:59:57 +0200

    Initial commit

--- src/main/java/net/kingproductions/splitbornAPI/CuriosityContainer/CuriosityCreator.java
+++ src/main/java/net/kingproductions/splitbornAPI/CuriosityContainer/CuriosityCreator.java
@@ -1,5 +1,6 @@
 package net.kingproductions.splitbornAPI.CuriosityContainer;
 
+import net.kingproductions.splitbornAPI.LocationsContainer.Locations;
 import org.bukkit.Material;
 
 import java.util.HashSet;
@@ -18,6 +19,7 @@ public class CuriosityCreator {
         CuriosityData curiosityData = new CuriosityData();
         curiosityData.setID(Curiosities.HEADLESS_EYE);
         curiosityData.setDisplayMaterial(Material.PLAYER_HEAD);
+        curiosityData.setLocation(Locations.Hearthgrove);
         curiosityData.setSkullValue("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjYwYTc3ZDJiN2ZjYWI5MTZkZDk1NjcwOGFhNTEwMTZjZjVhZmQwNDU5ZmNhMzQ2YmQ2OTgyZmI1ZDViOTQ0In19fQ==" );
 
         return curiosityData;
