261585d5ec8c3abcc05c36b05cddb12a759fd520
BuildTools <unconfigured@null.spigotmc.org>
2026-05-02 19:09:23 +0200

    Initial commit

--- src/main/java/net/kingproductions/splitbornAPI/RewardContainer/CURIOSITIES_REWARD_DATA.java
+++ src/main/java/net/kingproductions/splitbornAPI/RewardContainer/CURIOSITIES_REWARD_DATA.java
@@ -1,6 +1,11 @@
 package net.kingproductions.splitbornAPI.RewardContainer;
 
 import net.kingproductions.splitbornAPI.CuriosityContainer.Curiosities;
+import net.kingproductions.splitbornAPI.ItemContainer.Item_ID;
+import net.kingproductions.splitbornAPI.QuestContainer.Quests;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CURIOSITIES_REWARD_DATA {
 
@@ -15,6 +20,16 @@ public class CURIOSITIES_REWARD_DATA {
 
 
         return xpReward;
+    }
+
+    public static Map<Item_ID, Integer> getExtraRewards(Curiosities curiosity){
+        Map<Item_ID, Integer> map = new HashMap<>();
+
+        if (curiosity.equals(Curiosities.HEADLESS_EYE)){
+            map.put(Item_ID.FUNNY_SWORD, 1);
+        }
+
+        return map;
     }
 
 }
