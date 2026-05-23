eae6497657a8eeb5dd8a1fd8c466353e67ade200
BuildTools <unconfigured@null.spigotmc.org>
2026-05-24 00:31:20 +0200

    DB_Object

--- src/main/java/net/kingproductions/splitbornAPI/Util/UtilProvider.java
+++ src/main/java/net/kingproductions/splitbornAPI/Util/UtilProvider.java
@@ -20,6 +20,6 @@ public interface UtilProvider {
     Location getRandomSurfaceLocation(Location center, int radius);
     void getAllRegisteredUUID(Consumer<List<UUID>> callback);
     void setObjectIntoDB(String CollectionName, String ID, String oPath, Object o, boolean replace);
-    Object getDBObject(String CollectionName, String path);
+    Object getDBObject(String CollectionName, String oPath, String path);
 
 }
