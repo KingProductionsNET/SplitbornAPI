7aa8c1307a2a2c411cd867073b848a10845ad52e
BuildTools <unconfigured@null.spigotmc.org>
2026-05-30 19:18:39 +0200

    Range/Reel

--- src/main/java/net/kingproductions/splitbornAPI/MobContainer/RPGMobProvider.java
+++ src/main/java/net/kingproductions/splitbornAPI/MobContainer/RPGMobProvider.java
@@ -1,7 +1,9 @@
 package net.kingproductions.splitbornAPI.MobContainer;
 
+import org.bukkit.Location;
+
 public interface RPGMobProvider {
 
-    void spawnRPGMob(Mob_ID mobId);
+    void spawnRPGMob(Mob_ID mobId, Location spawnLocation);
 
 }
