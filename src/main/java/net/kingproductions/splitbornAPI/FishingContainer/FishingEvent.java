84215c186a459a1d1b08d1ac5bbc6ad32ad145c7
BuildTools <unconfigured@null.spigotmc.org>
2026-05-31 12:10:34 +0200

    Range/Reel

--- src/main/java/net/kingproductions/splitbornAPI/FishingContainer/FishingEvent.java
+++ src/main/java/net/kingproductions/splitbornAPI/FishingContainer/FishingEvent.java
@@ -4,41 +4,59 @@ import net.kingproductions.splitbornAPI.NPC.NPCInteractEvent;
 import net.kingproductions.splitbornAPI.NPC.NPC_ID;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class FishingEvent extends Event {
+public class FishingEvent extends Event implements Cancellable {
+
 
     private static final HandlerList handlers = new HandlerList();
 
     private final Player player;
-    private final boolean isAboutToCatchSomething_;
-    private boolean cancel;
+    private final boolean isAboutToCatchSomething;
+    private boolean cancelled;
 
-    public FishingEvent(Player player, boolean isAboutToCatchSomething_, boolean cancel) {
+    private Entity modifiedEntityDrop;
+    private ItemStack modifiedItemDrop;
+
+    public FishingEvent(Player player, boolean isAboutToCatchSomething, boolean cancelled, Entity modifiedEntityDrop, ItemStack modifiedItemDrop) {
         this.player = player;
-        this.isAboutToCatchSomething_ = isAboutToCatchSomething_;
-        this.cancel = cancel;
+        this.isAboutToCatchSomething = isAboutToCatchSomething;
+        this.cancelled = cancelled;
+        this.modifiedEntityDrop = modifiedEntityDrop;
+        this.modifiedItemDrop = modifiedItemDrop;
     }
 
     public Player getPlayer() {
         return player;
     }
-    public boolean isAboutToCatchSomething(){
-        return this.isAboutToCatchSomething_;
+
+    public boolean isAboutToCatchSomething() {
+        return isAboutToCatchSomething;
     }
 
-    public FishingEvent setCancelled(boolean b){
-        this.cancel = b;
-        return this;
+    public void setModifiedEntityDrop(Entity e){
+        this.modifiedEntityDrop = e;
+    }
+    public void setModifiedItemDrop(ItemStack i){
+        this.modifiedItemDrop = i;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 
     @Override
     public HandlerList getHandlers() {
-        return handlers;
-    }
-    public static HandlerList getHandlerList() {
         return handlers;
     }
 }
