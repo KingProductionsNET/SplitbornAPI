e77c785ba5d049081ce941f37e77da2e33ae5ff7
BuildTools <unconfigured@null.spigotmc.org>
2026-07-05 16:10:06 +0200

    A lot

--- src/main/java/net/kingproductions/splitbornAPI/HelperContainer/HelperProvider.java
+++ src/main/java/net/kingproductions/splitbornAPI/HelperContainer/HelperProvider.java
@@ -8,10 +8,7 @@ import net.kingproductions.splitbornAPI.MobContainer.Mob_ID;
 import net.kingproductions.splitbornAPI.RarityContainer.Rarities;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
@@ -88,5 +85,6 @@ public interface HelperProvider {
     Arrow spawnBowArrow(Player shooter, Location spawnLocation, Vector direction, int speed, boolean visibleForEveryone);
     boolean blockIsBetween(Location loc1, Location loc2);
     int getPlayersItemCountInWholeInventory(Player player, Item_ID itemId);
+    void applyDummyDamage(Player target, int Damage, String CustomName);
 
 }
