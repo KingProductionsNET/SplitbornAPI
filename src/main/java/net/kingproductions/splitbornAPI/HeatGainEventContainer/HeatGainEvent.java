b347c269704099ad51c91d6bd40dff0cd0e969b6
BuildTools <unconfigured@null.spigotmc.org>
2026-07-08 19:45:08 +0200

    A lot

---
+++ src/main/java/net/kingproductions/splitbornAPI/HeatGainEventContainer/HeatGainEvent.java
@@ -0,0 +1,39 @@
+package net.kingproductions.splitbornAPI.HeatGainEventContainer;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class HeatGainEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Player player;
+    private final int heatGainAmount;
+    private boolean cancelled;
+
+    public HeatGainEvent(Player player, int Amount){
+        this.player = player;
+        this.heatGainAmount = Amount;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        cancelled = b;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return null;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
