fcb34d4da4cd75a600cfc0f807eba23d84e6b215
BuildTools <unconfigured@null.spigotmc.org>
2026-07-09 15:19:16 +0200

    A lot

---
+++ src/main/java/net/kingproductions/splitbornAPI/BowShootEventContainer/BowShootEvent.java
@@ -0,0 +1,48 @@
+package net.kingproductions.splitbornAPI.BowShootEventContainer;
+
+import net.kingproductions.splitbornAPI.ItemContainer.Item_ID;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class BowShootEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Player player;
+    private final Item_ID itemId;
+    private boolean cancelled;
+
+    public BowShootEvent(Player receiver, Item_ID itemId, boolean cancelled){
+        this.player = receiver;
+        this.itemId = itemId;
+        this.cancelled = cancelled;
+    }
+
+    public Player getPlayer(){
+        return player;
+    }
+    public Item_ID getBowItemID(){return itemId;}
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancelled = b;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
