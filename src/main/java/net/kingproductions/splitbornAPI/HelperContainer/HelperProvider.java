78d0465d540041a0158180329fd169a43779bdcc
BuildTools <unconfigured@null.spigotmc.org>
2026-07-10 15:39:51 +0200

    A lot

--- src/main/java/net/kingproductions/splitbornAPI/HelperContainer/HelperProvider.java
+++ src/main/java/net/kingproductions/splitbornAPI/HelperContainer/HelperProvider.java
@@ -97,8 +97,8 @@ public interface HelperProvider {
     void applyHeat(Player player, int Amount);
     List<Item_ID> getPlayersEquippedBonusItems(Player player);
     void setItemCooldownForPlayer(Player player, Item_ID itemId, int Amount);
-    int getCooldownInSeconds(Item_ID itemId, int cooldown);
-    void sendCooldownMessage(Player player, int Cooldown);
+    int getCooldownInSeconds(Item_ID itemId);
+    void sendCooldownMessage(Player player, Item_ID itemId);
     boolean isImmortal(Entity e);
     MobCategory getMobCategory(Mob_ID mobId);
     Mob_ID getMobID(Entity e);
