5589b566199e409e609755d0abb3c683cad39b84
hamza-apps <hamza-apps@users.noreply.github.com>
2026-07-21 15:38:38 +0200

    A lot

--- src/main/java/net/kingproductions/splitbornAPI/HelperContainer/HelperProvider.java
+++ src/main/java/net/kingproductions/splitbornAPI/HelperContainer/HelperProvider.java
@@ -86,7 +86,7 @@ public interface HelperProvider {
     boolean bonusItemIsActive(Player player, Item_ID itemId);
     void fillInventory(Inventory inventory, Material material);
     Rarities getItemsRarity(ItemStack itemStack);
-    void applyRarityGlow(Item item, Player viewer, Rarities rarity);
+    void applyRarityGlow(Item item, Rarities rarity);
     boolean isWearing(Player player, Item_ID itemId);
     boolean isHolding(Player player, Item_ID itemId);
     boolean inventoryContains(Player player, Item_ID itemId);
