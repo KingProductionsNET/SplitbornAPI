1b4c38766b46701a6c37ee89a9abfc7f2a28ff0a
BuildTools <unconfigured@null.spigotmc.org>
2026-07-30 01:27:55 +0200

    A lot

--- src/main/java/net/kingproductions/splitbornAPI/LocationsContainer/LocationSpawnPoints.java
+++ src/main/java/net/kingproductions/splitbornAPI/LocationsContainer/LocationSpawnPoints.java
@@ -10,7 +10,7 @@ public class LocationSpawnPoints {
     public static Location The_Fold_Spawn = null;
 
     public static void InitializeLocations(){
-        Hearthgrove_Spawn = new Location(world_splitborn, 43, 94, 210, 90, -0);
+        Hearthgrove_Spawn = new Location(world_splitborn, 44.5, 94, 214.5, 164, 0);
         The_Fold_Spawn = new Location(world_splitborn, 43.5, 82, 180.5, -54, 1);
     }
 
