b9dca4ca2d5e7244ed06ed02e3a61eb37eeb70fb
BuildTools <unconfigured@null.spigotmc.org>
2026-08-01 00:50:24 +0200

    A lot

---
+++ src/main/java/net/kingproductions/splitbornAPI/CustomAttackEvent/CustomAttackEvent.java
@@ -0,0 +1,56 @@
+package net.kingproductions.splitbornAPI.CustomAttackEvent;
+
+import net.kingproductions.splitbornAPI.ItemContainer.Item_ID;
+import net.kingproductions.splitbornAPI.MobContainer.Mob_ID;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class CustomAttackEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Player player;
+    private final Mob_ID mobId;
+    private final Entity mob;
+    private boolean cancelled;
+
+    public CustomAttackEvent(Player receiver, Mob_ID mobId, Mob mob, boolean cancelled){
+        this.player = receiver;
+        this.mobId = mobId;
+        this.mob = mob;
+        this.cancelled = cancelled;
+    }
+
+    public Player getPlayer(){
+        return player;
+    }
+    public Mob_ID getMobId(){return mobId;}
+    public Entity getMob(){
+        return mob;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancelled = b;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
---
+++ src/main/java/net/kingproductions/splitbornAPI/EntityClickEventContainer/EntityClickEvent.java
@@ -0,0 +1,52 @@
+package net.kingproductions.splitbornAPI.EntityClickEventContainer;
+
+import net.kingproductions.splitbornAPI.MobContainer.Mob_ID;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class EntityClickEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Player player;
+    private final Mob_ID mobId;
+    private final Entity mob;
+    private final CLICK_TYPE clickType;
+
+    public EntityClickEvent(Player receiver, Mob_ID mobId, Mob mob, CLICK_TYPE clickType){
+        this.player = receiver;
+        this.mobId = mobId;
+        this.mob = mob;
+        this.clickType = clickType;
+    }
+
+    public Player getPlayer(){
+        return player;
+    }
+    public Mob_ID getMobId(){return mobId;}
+    public Entity getMob() {
+        return mob;
+    }
+    public CLICK_TYPE getClickType(){
+        return clickType;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public enum CLICK_TYPE {
+        LEFT,
+        RIGHT
+    }
+}
