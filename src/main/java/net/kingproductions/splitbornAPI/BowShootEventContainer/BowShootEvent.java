dd2c69e32c8816cf46819bf537210f8f7eaf530a
BuildTools <unconfigured@null.spigotmc.org>
2026-08-06 18:59:32 +0200

    A lot

--- src/main/java/net/kingproductions/splitbornAPI/BowShootEventContainer/BowShootEvent.java
+++ src/main/java/net/kingproductions/splitbornAPI/BowShootEventContainer/BowShootEvent.java
@@ -1,6 +1,7 @@
 package net.kingproductions.splitbornAPI.BowShootEventContainer;
 
 import net.kingproductions.splitbornAPI.ItemContainer.Item_ID;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
@@ -13,11 +14,14 @@ public class BowShootEvent extends Event implements Cancellable {
 
     private final Player player;
     private final Item_ID itemId;
+    private final Entity entity;
+    private boolean onlyShowArrowForShooter;
     private boolean cancelled;
 
-    public BowShootEvent(Player player, Item_ID itemId, boolean cancelled){
+    public BowShootEvent(Player player, Item_ID itemId, Entity projectile, boolean cancelled){
         this.player = player;
         this.itemId = itemId;
+        this.entity = projectile;
         this.cancelled = cancelled;
     }
 
@@ -25,6 +29,17 @@ public class BowShootEvent extends Event implements Cancellable {
         return player;
     }
     public Item_ID getBowItemID(){return itemId;}
+    public boolean getOnlyShowArrowForShooter(){
+        return onlyShowArrowForShooter;
+    }
+    public Entity getProjectile(){
+        return entity;
+    }
+
+    public void setOnlyShowArrowForShooter(boolean b){
+        this.onlyShowArrowForShooter = b;
+    }
+
 
     @Override
     public boolean isCancelled() {
