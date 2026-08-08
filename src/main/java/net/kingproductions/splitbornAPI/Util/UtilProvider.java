caa29ee295e9d8cc84b3c1f888e860b9b963ec31
BuildTools <unconfigured@null.spigotmc.org>
2026-08-09 00:51:01 +0200

    A lot

--- src/main/java/net/kingproductions/splitbornAPI/Util/UtilProvider.java
+++ src/main/java/net/kingproductions/splitbornAPI/Util/UtilProvider.java
@@ -32,7 +32,7 @@ public interface UtilProvider {
     void boostTo(Entity e, Location targetLoc, double strength);
     void arcMoveForPlayer(Player player, Location loc1, Location loc2, int speed, Consumer<Player> c, Consumer<Entity> shuttle);
 
-    void sendPacketBlock(Player player, Location location, Material material, boolean interactable);
+    void sendPacketBlock(Player player, Location location, Material material, boolean preventInteraction);
     void sendPacketMultiBlocks(Player player, List<Location> locations, Material material, boolean preventInteraction);
 
     void adjustPlayerGleams(UUID uuid, int delta);
