203237d88d7a732484e5b97911db2d751785ae14
BuildTools <unconfigured@null.spigotmc.org>
2026-08-17 21:56:09 +0200

    A lot

--- src/main/java/net/kingproductions/splitbornAPI/ItemContainer/Item_Category.java
+++ src/main/java/net/kingproductions/splitbornAPI/ItemContainer/Item_Category.java
@@ -2,6 +2,15 @@ package net.kingproductions.splitbornAPI.ItemContainer;
 
 public enum Item_Category {
 
-    ITEM,HELMET,CHESTPLATE,LEGGINGS,BOOTS,WEAPON,FISHING_ROD,BONUS_ITEM,MINING_TOOL
+    ITEM,
+    HELMET,
+    CHESTPLATE,
+    LEGGINGS,
+    BOOTS,
+    WEAPON,
+    FISHING_ROD,
+    BONUS_ITEM,
+    MINING_TOOL,
+    BOW
 
 }
