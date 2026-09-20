8c9953780f3fc243986a24ad45a9debbb299a484
hamza-apps <hamza-apps@users.noreply.github.com>
2026-09-20 14:10:06 +0200

    A lot

--- src/main/java/net/kingproductions/splitbornAPI/MiningContainer/Block_ID.java
+++ src/main/java/net/kingproductions/splitbornAPI/MiningContainer/Block_ID.java
@@ -10,21 +10,11 @@ public enum Block_ID {
     SUNSTONE,
     FROSTED_ICE,
     GLACIAL_ICE,
-
-    // Hollowdeep - Layer 1 (plain cave)
     GLOOMSTONE,
-
-    // Hollowdeep - Layer 2 (red terracotta / granite)
     RUSTVEIN_ORE,
     OCHRE_CLAY,
-
-    // Hollowdeep - Layer 3 (green, flooded)
     VERDANT_BLOOM,
-
-    // Hollowdeep - Layer 4 (pale)
     PALE_AMETHYST,
-
-    // Hollowdeep - Layer 5 (abandoned mine shaft)
     PROSPECTORS_EMERALD
 
 }
